7ab06cdf520047cf2f82f1d6bf16e9889ce58af0
주원 <juwon_0715@naver.com>
2026-04-11 22:09:02 +0900

    Mockito 사용

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -16,9 +16,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.PrivilegedAction;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
